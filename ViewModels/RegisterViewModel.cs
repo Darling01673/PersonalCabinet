8c650949236e8e10a7bf85824f9f5ddf666c5123
Darling <darling009@inbox.ru>
2026-06-02 20:27:29 +0400

    Исправления 3

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -11,9 +11,9 @@ namespace PersonalCabinet.ViewModels
         public string? OrgShortName { get; set; }
         public string? ContactPerson { get; set; }
 
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
 
         public bool PersonalDataConsent { get; set; }
     }
