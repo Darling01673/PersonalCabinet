7a589ce13074315aca4eb723267f95aec38fb2ea
Darling <darling009@inbox.ru>
2026-06-11 12:40:46 +0400

    Исправления бд

---
+++ Migrations/20260611083618_Nullable.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalCabinet.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+     name: "LastName",
+     table: "applications",
+     nullable: true,
+     oldClrType: typeof(string),
+     oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MiddleName",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResidenceAddress",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PassportSeries",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PassportNumber",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PassportWhoIssued",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressRegistr",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PassportDate",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "SNILS",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateSNILS",
+                table: "applications",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: false);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
